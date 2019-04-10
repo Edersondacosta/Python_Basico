e6ad73bfe4a81b241f5ff6c596f3cbc205a7f6de
Edersondacosta <34005488+Edersondacosta@users.noreply.github.com>
2019-04-09 23:08:21 -0300

    Add files via upload

---
+++ media_aluno.py
@@ -0,0 +1,23 @@
+tab = int(input("Digite o valor da tabuada= "))
+
+
+v1 = (tab*1)
+print ("O valor X 1= ", v1)
+v2 = (tab*2)
+print ("O valor X 2=", v2)
+v3 = (tab*3)
+print ("O valor X 3=", v3)
+v4 = (tab*4)
+print ("O valor X 4= ", v4)
+v5 = (tab*5)
+print ("O valor X 5=", v5)
+v6 = (tab*6)
+print ("O valor X 6=", v6)
+v7 = (tab*7)
+print ("O valor X 7=", v7)
+v8 = (tab*8)
+print ("O valor X 8=", v8)
+v9 = (tab*9)
+print ("O valor X 9=", v9)
+
+
